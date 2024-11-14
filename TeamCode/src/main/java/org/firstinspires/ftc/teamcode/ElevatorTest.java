24860b132e412df8a6484dffb0807b75d328c90f
quizque <13785390+quizque@users.noreply.github.com>
2024-11-14 00:00:42 -0500

    Better elevator controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorTest.java
@@ -7,6 +7,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -25,15 +26,11 @@ public class ElevatorTest extends OpMode {
 
     private DcMotorEx elevatorLeft;
     private DcMotorEx elevatorRight;
-    private Elevator elevator;
 
-    PIDController pidLeft;
-    PIDController pidRight;
+    PIDFController pidLeft;
+    PIDFController pidRight;
 
     private double targetHeight = 0.0;
-
-    static double l_smoothed = 0.0;
-    static double r_smoothed = 0.0;
 
     @Override
     public void init() {
@@ -56,44 +53,50 @@ public class ElevatorTest extends OpMode {
         elevatorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         elevatorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        pidLeft = new PIDController(PARAMS.kP, PARAMS.kI, PARAMS.kD);
-        pidRight = new PIDController(PARAMS.kP, PARAMS.kI, PARAMS.kD);
-
+        pidLeft = new PIDFController(PARAMS.kP, PARAMS.kI, PARAMS.kD, PARAMS.kG);
+        pidRight = new PIDFController(PARAMS.kP, PARAMS.kI, PARAMS.kD, PARAMS.kG);
     }
+
+    long timestamp = System.currentTimeMillis() / 1000;
 
     @Override
     public void loop() {
         TelemetryPacket packet = new TelemetryPacket();
 
-        targetHeight += gamepad1.right_trigger * PARAMS.controlConst;
-        targetHeight -= gamepad1.left_trigger * PARAMS.controlConst;
-
-//            elevatorLeft.setPower(gamepad.right_trigger + PARAMS.kG);
-//            elevatorRight.setPower(gamepad.right_trigger + PARAMS.kG);
+        if (gamepad1.a) {
+            targetHeight = 0;
+            timestamp = System.currentTimeMillis() / 1000;
+        } else if (gamepad1.b) {
+            targetHeight = 1000;
+                    timestamp = System.currentTimeMillis() / 1000;
+        }else if (gamepad1.x) {
+            targetHeight = 2500;
+                    timestamp = System.currentTimeMillis() / 1000;
+        }else if (gamepad1.y) {
+            targetHeight = 4000;
+                    timestamp = System.currentTimeMillis() / 1000;
+        }
 
         targetHeight = Math.min(Math.max(PARAMS.encoderMinimum, targetHeight), PARAMS.encoderMaximum);
-
         pidLeft.setP(PARAMS.kP);
         pidLeft.setI(PARAMS.kI);
         pidLeft.setD(PARAMS.kD);
+        pidLeft.setF(PARAMS.kG);
 
         pidRight.setP(PARAMS.kP);
         pidRight.setI(PARAMS.kI);
         pidRight.setD(PARAMS.kD);
+        pidRight.setF(PARAMS.kG);
 
         // Drive motors
-        double l = pidLeft.calculate(elevatorLeft.getCurrentPosition(), targetHeight) + PARAMS.kG;
-        double r = pidRight.calculate(elevatorRight.getCurrentPosition(), targetHeight) + PARAMS.kG;
+        double l = pidLeft.calculate(elevatorLeft.getCurrentPosition(), targetHeight);
+        double r = pidRight.calculate(elevatorRight.getCurrentPosition(), targetHeight) ;
 
-        l_smoothed = PARAMS.alpha * l_smoothed + (1-PARAMS.alpha) * l;
-        r_smoothed = PARAMS.alpha * r_smoothed + (1-PARAMS.alpha) * r;
+        l = Math.min(Math.max(PARAMS.minSpeed, l), PARAMS.maxSpeed);
+        r = Math.min(Math.max(PARAMS.minSpeed, r), PARAMS.maxSpeed);
 
-        l = Math.min(Math.max(PARAMS.minSpeed, l_smoothed), PARAMS.maxSpeed);
-        r = Math.min(Math.max(PARAMS.minSpeed, r_smoothed), PARAMS.maxSpeed);
-
-
-        packet.put("pid_left", l_smoothed);
-        packet.put("pid_right", r_smoothed);
+        packet.put("pid_left", l);
+        packet.put("pid_right", r);
         packet.put("encoder_left", elevatorLeft.getCurrentPosition());
         packet.put("encoder_right", elevatorRight.getCurrentPosition());
         packet.put("elevator-target", targetHeight);
@@ -105,19 +108,15 @@ public class ElevatorTest extends OpMode {
 
     public static class Params {
         // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/introduction-to-feedforward.html#elevator-feedforward
-        public double kG = 0.175;
+        public double kG = 0.1;
 
         // Normal PID constants
         public double kP = 0.005;
-        public double kI = 0.01;
-        public double kD = 0.0005;
+        public double kI = 0.0;
+        public double kD = 0.0001;
 
         public double maxSpeed = 1.0;
-        public double minSpeed = -0.01;
-
-        public double alpha = 0.1;
-
-        public double controlConst = 20.0;
+        public double minSpeed = -0.3;
 
         // Elevator height constants (in units of encoder ticks)
         public double encoderMinimum = 0.0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorTestSingleEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorTestSingleEncoder.java
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @Config
 public class ElevatorTestSingleEncoder extends OpMode {
 
-    public static ElevatorTestSingleEncoder.Params PARAMS = new ElevatorTestSingleEncoder.Params();
+    public static ElevatorTest.Params PARAMS = new ElevatorTest.Params();
     private FtcDashboard dash = FtcDashboard.getInstance();
 
     private DcMotorEx elevatorLeft;
@@ -26,6 +26,9 @@ public class ElevatorTestSingleEncoder extends OpMode {
     PIDController pidRight;
 
     private double targetHeight = 0.0;
+    private double targetHeightSmooted = 0.0;
+
+    private TrapezoidalMotionProfile tProfile = new TrapezoidalMotionProfile(0.0, 0.0, 0.0, 0.0);
 
     @Override
     public void init() {
@@ -35,8 +38,8 @@ public class ElevatorTestSingleEncoder extends OpMode {
         elevatorRight = hardwareMap.get(DcMotorEx.class, "elevatorRight");
 
         // Set idle behavior
-//        elevatorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        elevatorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevatorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevatorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Reverse motor direction
         elevatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -45,68 +48,44 @@ public class ElevatorTestSingleEncoder extends OpMode {
         elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevatorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevatorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         pidLeft = new PIDController(PARAMS.kP, PARAMS.kI, PARAMS.kD);
         pidRight = new PIDController(PARAMS.kP, PARAMS.kI, PARAMS.kD);
 
     }
 
+    long timestamp = System.currentTimeMillis() / 1000;
+    double currPos = 0.0;
+
     @Override
     public void loop() {
         TelemetryPacket packet = new TelemetryPacket();
 
-//        targetHeight += ;
-//        targetHeight -= ;
-
-            elevatorLeft.setPower(gamepad1.right_trigger * PARAMS.contC - gamepad1.left_trigger * PARAMS.contC);
-            elevatorRight.setPower(gamepad1.right_trigger * PARAMS.contC - gamepad1.left_trigger * PARAMS.contC);
-
-        targetHeight = Math.min(Math.max(PARAMS.encoderMinimum, targetHeight), PARAMS.encoderMaximum);
-
-        pidLeft.setP(PARAMS.kP);
-        pidLeft.setI(PARAMS.kI);
-        pidLeft.setD(PARAMS.kD);
-
-        pidRight.setP(PARAMS.kP);
-        pidRight.setI(PARAMS.kI);
-        pidRight.setD(PARAMS.kD);
-
-        // Drive motors
-//        double l = pidLeft.calculate(elevatorLeft.getCurrentPosition(), targetHeight) + PARAMS.kG;
-        double r = pidRight.calculate(elevatorRight.getCurrentPosition(), targetHeight) + PARAMS.kG;
-
-
-
-//        packet.put("pid_left", l);
-        packet.put("pid_right", r);
-        packet.put("encoder_left", elevatorLeft.getCurrentPosition());
-        packet.put("encoder_right", elevatorRight.getCurrentPosition());
-        packet.put("elevator-target", targetHeight);
-//        elevatorLeft.setPower(r);
-//        elevatorRight.setPower(r);
+        elevatorLeft.setPower(gamepad1.left_stick_y);
+        elevatorRight.setPower(gamepad1.left_stick_y);
 
         dash.sendTelemetryPacket(packet);
     }
 
     public static class Params {
         // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/introduction-to-feedforward.html#elevator-feedforward
-        public double kG = 0.0;
+        public double kG = 0.1;
 
         // Normal PID constants
-        public double kP = 0.0;
+        public double kP = 0.005;
         public double kI = 0.0;
-        public double kD = 0.0;
+        public double kD = 0.0001;
 
-        public double maxSpeed = 0.5;
-        public  double minSpeed = 0.1;
 
-        public double contC = 0.5;
+
+        public double maxSpeed = 1.0;
+        public double minSpeed = -0.3;
 
         // Elevator height constants (in units of encoder ticks)
         public double encoderMinimum = 0.0;
-        public double encoderMaximum = 0.0;
+        public double encoderMaximum = 4000.0;
     }
 
 }
