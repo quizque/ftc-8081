4dd7db789c6ee2450aa6b31218bd8c44c5349d08
Nick <13785390+quizque@users.noreply.github.com>
2024-11-24 17:07:01 -0500

    wdaw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
@@ -1,17 +1,11 @@
 package org.firstinspires.ftc.teamcode;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
-
-import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -23,17 +17,23 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
  */
 @Config
 @Autonomous(group = "drive")
-public class Auto1 extends LinearOpMode {
-    public static double DISTANCE = 60; // in
+public class LeftSideAuto extends LinearOpMode {
+    private Elevator elevator;
+    private Grabber grabber;
 
     @Override
     public void runOpMode() throws InterruptedException {
         Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        elevator = new Elevator(hardwareMap);
+        grabber = new Grabber(hardwareMap);
 
         TrajectorySequence trajectory0 = drive.trajectorySequenceBuilder(new Pose2d(-39.56, -62.50, Math.toRadians(90.00)))
                 .splineTo(new Vector2d(-47.83, -47.83), Math.toRadians(45.00))
+                .build();
+
+        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d(-48.03, -48.03, Math.toRadians(45.00)))
                 .lineToConstantHeading(new Vector2d(-60.80, -58.84))
                 .build();
 
@@ -44,6 +44,9 @@ public class Auto1 extends LinearOpMode {
         if (isStopRequested()) return;
 
         drive.followTrajectorySequence(trajectory0);
+        elevator.setHeight(4000);
+        while (!elevator.atTarget()) {}
+        drive.followTrajectorySequence(traj1);
 
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("finalX", poseEstimate.getX());
