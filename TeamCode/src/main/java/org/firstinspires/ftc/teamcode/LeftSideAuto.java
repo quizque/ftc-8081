e57e3f3612bf275ce4cc969c63ab5a66939d3152
Nick <13785390+quizque@users.noreply.github.com>
2024-11-24 18:12:10 -0500

    Update LeftSideAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
@@ -7,6 +7,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -38,7 +39,22 @@ public class LeftSideAuto extends LinearOpMode {
 
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(trajectory0.end())
                 .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
-                .lineToConstantHeading(new Vector2d(-60.80, -58.84))
+                .lineToConstantHeading(new Vector2d(-58.44, -59.43))
+                .build();
+
+        TrajectorySequence traj2 = drive.trajectorySequenceBuilder(traj1.end())
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
+                .lineToConstantHeading(new Vector2d(-47.83, -47.83))
+                .build();
+
+        TrajectorySequence traj3 = drive.trajectorySequenceBuilder(traj2.end())
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
+                .turn(Math.toRadians(90.0))
+                .build();
+
+        TrajectorySequence traj4 = drive.trajectorySequenceBuilder(traj2.end())
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
+                .turn(Math.toRadians(90.0))
                 .build();
 
         drive.setPoseEstimate(trajectory0.start());
@@ -49,6 +65,8 @@ public class LeftSideAuto extends LinearOpMode {
         drive.followTrajectorySequenceAsync(trajectory0);
         int state = 0;
 
+        Timing.Timer timer = new Timing.Timer(3);
+
         while (opModeIsActive() && !isStopRequested()) {
 
             elevator.run();
@@ -58,7 +76,6 @@ public class LeftSideAuto extends LinearOpMode {
             if (state == 0) {
                 if (!drive.isBusy()) {
                     state++;
-
                     elevator.setHeight(4000);
                 }
             } else if (state == 1) {
@@ -66,8 +83,35 @@ public class LeftSideAuto extends LinearOpMode {
                     state++;
                     drive.followTrajectorySequenceAsync(traj1);
                 }
-            } else if (state == 2)
-            {
+            } else if (state == 2) {
+
+                if (!drive.isBusy()) {
+                    grabber.intakeOut();
+                    drive.followTrajectorySequenceAsync(traj2);
+                    state++;
+                }
+            } else if (state == 3) {
+                if (!drive.isBusy()) {
+                    grabber.intakeStop();
+                    state++;
+                    elevator.setHeight(0);
+                    drive.followTrajectorySequenceAsync(traj3);
+                }
+            } else if (state == 4) {
+                if (!drive.isBusy() && elevator.atTarget()) {
+                    state++;
+                    grabber.slideToOutside();
+                    grabber.armToFloor();
+                    grabber.intakeIn();
+                    timer.start();
+                }
+            } else if (state == 5) {
+                if (timer.done()) {
+                    grabber.intakeStop();
+                    grabber.armToInside();
+                    grabber.slideToInside();
+                    state++;
+                }
             }
 
 
