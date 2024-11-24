125c17660f7a44cf6ffab252c27d580c82b390b2
Nick <13785390+quizque@users.noreply.github.com>
2024-11-24 17:32:54 -0500

    jh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
@@ -33,6 +33,7 @@ public class Auto1 extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         TrajectorySequence trajectory0 = drive.trajectorySequenceBuilder(new Pose2d(-39.56, -62.50, Math.toRadians(90.00)))
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
                 .splineTo(new Vector2d(-47.83, -47.83), Math.toRadians(45.00))
                 .lineToConstantHeading(new Vector2d(-60.80, -58.84))
                 .build();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -30,10 +32,12 @@ public class LeftSideAuto extends LinearOpMode {
         grabber = new Grabber(hardwareMap);
 
         TrajectorySequence trajectory0 = drive.trajectorySequenceBuilder(new Pose2d(-39.56, -62.50, Math.toRadians(90.00)))
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
                 .splineTo(new Vector2d(-47.83, -47.83), Math.toRadians(45.00))
                 .build();
 
-        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d(-48.03, -48.03, Math.toRadians(45.00)))
+        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(trajectory0.end())
+                .setConstraints(SampleMecanumDrive.getVelocityConstraint(30, 30, TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(15))
                 .lineToConstantHeading(new Vector2d(-60.80, -58.84))
                 .build();
 
@@ -42,18 +46,40 @@ public class LeftSideAuto extends LinearOpMode {
         waitForStart();
 
         if (isStopRequested()) return;
+        drive.followTrajectorySequenceAsync(trajectory0);
 
-        drive.followTrajectorySequence(trajectory0);
-        elevator.setHeight(4000);
-        while (!elevator.atTarget()) {}
-        drive.followTrajectorySequence(traj1);
+        int state = 0;
 
-        Pose2d poseEstimate = drive.getPoseEstimate();
-        telemetry.addData("finalX", poseEstimate.getX());
-        telemetry.addData("finalY", poseEstimate.getY());
-        telemetry.addData("finalHeading", poseEstimate.getHeading());
-        telemetry.update();
+        while (opModeIsActive() && !isStopRequested()) {
 
-        while (!isStopRequested() && opModeIsActive()) ;
+            elevator.run();
+            grabber.run();
+
+            switch (state) {
+                case 0:
+                    if (!drive.isBusy()) {
+                        state++;
+
+                        elevator.setHeight(4000);
+                    }
+                    break;
+                case 1:
+                    if (elevator.atTarget()) {
+                        state++;
+                        drive.followTrajectorySequenceAsync(traj1);
+                    }
+                    break;
+                case 2:
+                    break;
+            }
+
+
+            Pose2d poseEstimate = drive.getPoseEstimate();
+            telemetry.addData("finalX", poseEstimate.getX());
+            telemetry.addData("finalY", poseEstimate.getY());
+            telemetry.addData("finalHeading", poseEstimate.getHeading());
+            telemetry.update();
+
+        }
     }
 }
